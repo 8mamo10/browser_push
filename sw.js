bec7ec9972f566ce5017317ab8fe50ace2367865
Hiroki Yamamoto <562105+8mamo10@users.noreply.github.com>
2026-05-06 23:41:16 +0700

    Separate sw.js

---
+++ sw.js
@@ -0,0 +1,14 @@
+// sw.js
+self.addEventListener("install", (event) => {
+  console.log("Service Worker: Installed");
+});
+
+self.addEventListener("notificationclick", (event) => {
+  event.notification.close();
+  event.waitUntil(
+    clients.matchAll({ type: "window" }).then((clientList) => {
+      if (clientList.length > 0) return clientList[0].focus();
+      return clients.openWindow("/");
+    }),
+  );
+});
